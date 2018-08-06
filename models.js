0ef7685090463585b471b087459183423960516a
Shane EC Davis <secd0183@yahoo.com>
2018-08-06 16:40:18 -0500

    I dont know

--- models.js
+++ models.js
@@ -1,50 +1,53 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 const db = new Sequelize('postgres://localhost:5432/plantr', {
-    logging: false
-});
+  logging: false
+})
 
 const gardener = db.define('gardener', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    age: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    }
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
 })
 
 const plot = db.define('plot', {
-    size: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    shaded: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false
-    }
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
 })
 
 const vegetable = db.define('vegetable', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    color: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    planted_on: {
-        type: SEquelize.DATE,
-        allowNull: false
-    }
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  planted_on: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
 })
 
-gardener.hasOne(plot);
-gardener.belongsTo(vegetable, {as: 'favorite_vegetable'});
-vegetable.belongsToMany(plot, {through: 'vegetable_plot'});
-plot.belongsToMany(vegetable, {through: 'vegetable_plot'});
 
-db.sync()
+
+
+gardener.hasOne(plot)
+gardener.belongsTo(vegetable, { as: 'favorite_vegetable' })
+vegetable.belongsToMany(plot, { through: 'vegetable_plot' })
+plot.belongsToMany(vegetable, { through: 'vegetable_plot' })
+
+db.sync().then()
 
 module.exports = { db, gardener, plot, vegetable }
